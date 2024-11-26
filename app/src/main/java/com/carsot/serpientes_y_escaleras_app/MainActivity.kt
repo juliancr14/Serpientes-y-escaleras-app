4dd4ac5e6b0c7a520e552b9d753e11e7975d0781
juliancr14 <juliancardonarojas3@gmail.com>
2024-11-26 14:22:54 -0500

    Changed the package name

---
+++ app/src/main/java/com/carsot/serpientes_y_escaleras_app/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.carsot.serpientes_y_escaleras_app
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.carsot.serpientes_y_escaleras_app.ui.theme.SerpientesyescalerasappTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            SerpientesyescalerasappTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    SerpientesyescalerasappTheme {
+        Greeting("Android")
+    }
+}
